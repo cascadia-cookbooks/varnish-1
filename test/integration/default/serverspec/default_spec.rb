00c14cbfc1f86fdbd58ad564d9ba06e43e71b8d5
Colby Olson <me@colbyolson.com>
2016-11-28 20:17:00 -0800

    add more tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,17 +1,40 @@
 require 'spec_helper'
 
 describe 'cop_varnish::default' do
-  describe package('varnish') do
-    it { should be_installed }
+  describe file('/usr/local/sbin/varnishd') do
+    it { should be_executable }
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode '755' }
   end
 
-  describe command('varnishd -V') do
-    its(:stderr) { should match /4.?.*/ }
+  describe command('/usr/local/sbin/varnishd -V') do
+    its(:stderr) { should match /4.1.3/ }
   end
 
   describe file('/etc/varnish/default.vcl') do
+    it { should be_file }
     it { should be_owned_by 'root' }
     it { should be_grouped_into 'root' }
     it { should be_mode '644' }
+  end
+
+  describe file('/etc/varnish/secret') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode '600' }
+  end
+
+  describe file('/var/lib/varnish') do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_mode '755' }
+  end
+
+  describe service('varnish') do
+    it { should be_running }
   end
 end
