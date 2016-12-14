86e40d66cdb8cdf920dc6e973e02e4a3b75997b0
Colby Olson <me@colbyolson.com>
2016-12-14 11:12:50 -0800

    bump to 1.2.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures varnish.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.2.1'
+version '1.2.2'
 
 source_url 'https://github.com/copious-cookbooks/varnish'
 issues_url 'https://github.com/copious-cookbooks/varnish/issues'
