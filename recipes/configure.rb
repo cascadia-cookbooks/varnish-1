316967de60a3b073a450ef0cd914cbeee24715ce
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-17 15:47:28 -0700

    Update configuration with systemd and legacy templates

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -34,34 +34,60 @@ templates.each do |f,t|
     end
 end
 
-template '/etc/init.d/varnish' do
-    cookbook 'cop_varnish'
-    source   'varnish.init.erb'
-    group    'root'
-    owner    'root'
-    mode     0755
-    backup   5
-    action   :create
-    not_if   'ps -p 1 -o comm= | grep systemd'
-    notifies :restart, 'service[varnish]', :delayed
+# Select varnish service configuration template by platform
+template_source = value_for_platform(
+    %w(centos redhat) => {
+        'default' => 'varnish.service.systemd.erb'
+    },
+    'debian' => {
+        'default' => 'varnish.service.systemd.erb'
+    },
+    'ubuntu' => {
+        '14.04' => 'varnish.service.legacy.erb',
+        'default' => 'varnish.service.systemd.erb'
+    }
+)
+
+# Select varnish service configuration destination by platform
+template_destination = value_for_platform(
+    %w(centos redhat) => {
+        'default' => '/etc/sysconfig/varnish'
+    },
+    'debian' => {
+        'default' => '/etc/systemd/system/varnish.service.d/customexec.conf'
+    },
+    'ubuntu' => {
+        '14.04' => '/etc/default/varnish',
+        'default' => '/etc/systemd/system/varnish.service.d/customexec.conf'
+    }
+)
+
+# Create parent directory for template
+if node['platform_family'] == 'debian' && node['platform_version'] != '14.04'
+    directory '/etc/systemd/system/varnish.service.d/' do
+        owner 'root'
+        group 'root'
+        mode  0755
+    end
 end
 
-template '/usr/lib/systemd/system/varnish.service' do
+# Create template for varnish sevice configuration
+template template_destination do
     cookbook 'cop_varnish'
-    source   'varnish.service.erb'
+    source   template_source
     group    'root'
     owner    'root'
     mode     0644
-    backup   5
     action   :create
-    only_if  'ps -p 1 -o comm= | grep systemd'
     notifies :run, 'execute[reload systemctl]', :immediately
     notifies :restart, 'service[varnish]', :delayed
 end
 
+# Reload systemctl daemon
 execute 'reload systemctl' do
     command 'systemctl daemon-reload'
     action  :nothing
+    not_if node['platform_version'] == '14.04'
 end
 
 cookbook_file '/etc/logrotate.d/varnish' do
