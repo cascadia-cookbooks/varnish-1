2163ab03b62bff75bba0278dbda9143fca92be22
Hardy Johnson <hardyj@copiousinc.com>
2017-08-23 12:19:27 -0700

    Bump meta

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@copiousinc.com'
 license 'MIT'
 description 'Installs and configures varnish.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.1'
+version '3.0.2'
 
 source_url 'https://github.com/copious-cookbooks/varnish'
 issues_url 'https://github.com/copious-cookbooks/varnish/issues'
